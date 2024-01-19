6f9155401a1a2b590edf93bf1acd0531426cd96c
AbaaKil_35 <ab.abaakil@gmail.com>
2024-01-19 03:03:58 +0100

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,19 +1,67 @@
-import tkinter 
-from tkinter import*
+import tkinter
+from tkinter import *
+from tkinter import messagebox
 
+Calc = Tk()
+Calc.title("Calculatrice")
+Calc.geometry("600x600") 
+Calc.resizable(False, False)
+Calc.configure(bg="#051923")
 
-root = Tk()
-root.title("Calculator")
-root.geometry("600x600") 
-root.resizable(True, True)
-root.configure(bg="#17161b")
+equation = ""
+def show(value):
+    global equation
+    equation += value
+    label_result.config(text=equation)
+    
+def clear():
+    global equation
+    equation = ""
+    label_result.config(text=equation)
 
-label_result = Label(root, width=25, height=2, text="", font=("Arial", 30))
+def calculate():
+    global equation
+    result = ""
+    if equation != "":
+        try:
+            result = eval(equation)
+        except:
+            result = "Error"
+            equation = ""
+            messagebox.showerror("Error",  "Please enter a Number.")
+    label_result.config(text=result)
+                    
+                                  
+
+label_result = Label(Calc, width=25, height=2, text="", font=("Arial", 30),bg="#DBD2E0")
 label_result.pack()
 
-Button(root, text="C", width=5, height=1, font=("Arial", 30, "bold"), bd=1, fg="#fff", bg="#3697f5").place(x=20, y=100)
-Button(root, text="⌦", width=5, height=1, font=("Arial", 30, "bold"), bd=1, fg="#fff", bg="#3697f5").place(x=160, y=100)
-Button(root, text="%", width=5, height=1, font=("Arial", 30, "bold"), bd=1, fg="#fff", bg="#3697f5").place(x=300, y=100)
-Button(root, text="÷", width=5, height=1, font=("Arial", 30, "bold"), bd=1, fg="#fff", bg="#3697f5").place(x=440, y=100)
+Button(Calc, text="⌦", width=5, height=1, font=("Arial", 30, "bold"), bd=3, fg="#fff", bg="#463F3A",command=lambda: clear()).place(x=20, y=110)
+Button(Calc, text="÷", width=5, height=1, font=("Arial", 30, "bold"), bd=3, fg="#fff", bg="#463F3A",command=lambda: show('/')).place(x=160, y=110)
+Button(Calc, text="%", width=5, height=1, font=("Arial", 30, "bold"), bd=3, fg="#fff", bg="#463F3A",command=lambda: show('%')).place(x=300, y=110)
+Button(Calc, text="× ", width=5, height=1, font=("Arial", 30, "bold"), bd=3, fg="#fff", bg="#463F3A",command=lambda: show('*')).place(x=440, y=110)
 
-root.mainloop()
+
+Button(Calc, text="7", width=5, height=1, font=("Arial", 30, "bold"), bd=1, fg="#fff", bg="#273E47",command=lambda: show('7')).place(x=20, y=210)
+Button(Calc, text="8", width=5, height=1, font=("Arial", 30, "bold"), bd=1, fg="#fff", bg="#273E47",command=lambda: show('8')).place(x=160, y=210)
+Button(Calc, text="9", width=5, height=1, font=("Arial", 30, "bold"), bd=1, fg="#fff", bg="#273E47",command=lambda: show('9')).place(x=300, y=210)
+Button(Calc, text="-", width=5, height=1, font=("Arial", 30, "bold"), bd=3, fg="#fff", bg="#463F3A",command=lambda: show('-')).place(x=440, y=210)
+
+Button(Calc, text="4", width=5, height=1, font=("Arial", 30, "bold"), bd=1, fg="#fff", bg="#273E47",command=lambda: show('4')).place(x=20, y=300)
+Button(Calc, text="5", width=5, height=1, font=("Arial", 30, "bold"), bd=1, fg="#fff", bg="#273E47",command=lambda: show('5')).place(x=160, y=300)
+Button(Calc, text="6", width=5, height=1, font=("Arial", 30, "bold"), bd=1, fg="#fff", bg="#273E47",command=lambda: show('6')).place(x=300, y=300)
+Button(Calc, text="+", width=5, height=1, font=("Arial", 30, "bold"), bd=3, fg="#fff", bg="#463F3A",command=lambda: show('+')).place(x=440, y=300)
+
+
+Button(Calc, text="1", width=5, height=1, font=("Arial", 30, "bold"), bd=1, fg="#fff", bg="#273E47",command=lambda: show('1')).place(x=20, y=390)
+Button(Calc, text="2", width=5, height=1, font=("Arial", 30, "bold"), bd=1, fg="#fff", bg="#273E47",command=lambda: show('2')).place(x=160, y=390)
+Button(Calc, text="3", width=5, height=1, font=("Arial", 30, "bold"), bd=1, fg="#fff", bg="#273E47",command=lambda: show('3')).place(x=300, y=390)
+
+Button(Calc, text="0", width=11, height=1, font=("Arial", 30, "bold"), bd=1, fg="#fff", bg="#273E47",command=lambda: show('0')).place(x=20, y=480)
+Button(Calc, text=".", width=5, height=1, font=("Arial", 30, "bold"), bd=1, fg="#fff", bg="#273E47",command=lambda: show('.')).place(x=300, y=480)
+Button(Calc, text="=", width=5, height=3, font=("Arial", 30, "bold"), bd=3, fg="#fff", bg="#D8973C",command=lambda: calculate()).place(x=440, y=390)
+
+
+
+
+Calc.mainloop()
