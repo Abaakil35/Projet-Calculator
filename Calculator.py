2ff1b673edc6fc6d4cf68cefb94848ae4a36bedc
AbaaKil_35 <ab.abaakil@gmail.com>
2024-01-20 01:05:29 +0100

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -37,8 +37,8 @@ def calcul():
 label_result = Label(Calc, width=25, height=2, text="", font=("inherit", 30),bg="#E2FCEF")
 label_result.pack()
 
-Button(Calc, text="÷", width=7, height=1, font=("inherit", 30), bd=4,  fg="#fff", bg="#463F3A",command=lambda: voir('/')).place(x=20, y=110)
-Button(Calc, text="%", width=8, height=1, font=("inherit", 30), bd=4,   fg="#fff", bg="#463F3A",command=lambda: voir('%')).place(x=200, y=110)
+Button(Calc, text="⌦", width=7, height=1, font=("inherit", 30), bd=4,  fg="#fff", bg="#FC7753",command=lambda: suppr() ).place(x=20, y=110)
+Button(Calc, text="÷", width=8, height=1, font=("inherit", 30), bd=4,   fg="#fff", bg="#463F3A",command=lambda: voir('/')).place(x=200, y=110)
 Button(Calc, text="× ", width=7, height=1, font=("inherit", 30), bd=4,   fg="#fff", bg="#463F3A",command=lambda: voir('*')).place(x=400, y=110)
 
 
@@ -57,7 +57,7 @@ Button(Calc, text="1", width=5, height=1, font=("inherit", 30), bd=1, fg="#fff",
 Button(Calc, text="2", width=5, height=1, font=("inherit", 30), bd=1,  fg="#fff", bg="#273E47",command=lambda: voir('2')).place(x=160, y=390)
 Button(Calc, text="3", width=5, height=1, font=("inherit", 30), bd=1,  fg="#fff", bg="#273E47",command=lambda: voir('3')).place(x=300, y=390)
 
-Button(Calc, text="⌦", width=5, height=1, font=("inherit", 30 ), bd=4,  fg="#fff", bg="#463F3A",command=lambda: suppr()).place(x=20, y=480)
+Button(Calc, text="+/-", width=5, height=1, font=("inherit", 30 ), bd=1,  fg="#fff", bg="#273E47",command=lambda: voir('-')).place(x=20, y=480)
 Button(Calc, text="0", width=5, height=1, font=("inherit", 30), bd=1,  fg="#fff", bg="#273E47",command=lambda: voir('0')).place(x=160, y=480)
 Button(Calc, text=".", width=5, height=1, font=("inherit", 30), bd=1,  fg="#fff", bg="#273E47",command=lambda: voir('.')).place(x=300, y=480)
 Button(Calc, text="=", width=5, height=3, font=("inherit", 30), bd=4,  fg="#fff", bg="#FC7753",command=lambda: calcul()).place(x=440, y=390)
